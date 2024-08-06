97742cf4963874ef6778860ee0cd1d06df2d03f9
emin-hamidov <emin.hemidov@gmail.com>
2024-08-07 00:42:58 +0400

    SQL

---
+++ main.py
@@ -0,0 +1,12 @@
+# version: '3.8'
+# services:
+#   db:
+#     image: mysql
+#     envionment:
+#       MYSQL_ROOT_PASSWORD: "12345"
+#   ports:
+#     -"3306:3306"
+#   adminer:
+#    image: adminer  
+#    ports:
+#     -"8080:8080"
